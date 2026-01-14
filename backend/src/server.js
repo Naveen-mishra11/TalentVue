debdcafe2d01698e240fe7895657f0d5516eeb0b
Naveen Mishra <nvmkr12345@gmail.com>
2026-01-14 14:59:29 +0530

    session routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,9 @@ import {clerkMiddleware} from "@clerk/express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+
 import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
 
@@ -23,6 +25,7 @@ app.use(clerkMiddleware()); // this add auth info to req object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat",chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
