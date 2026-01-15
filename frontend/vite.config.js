14ae9c9db1005f99bc62ba6ba387c63034fac040
Naveen Mishra <nvmkr12345@gmail.com>
2026-01-15 23:34:15 +0530

    dashboard page added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    // ✅ Force Vite to pre-bundle react-resizable-panels correctly
-    include: ["react-resizable-panels"]
-  }
 })
